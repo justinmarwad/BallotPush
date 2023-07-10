33903fe82cc34a143f357eb694abcdb51bb80350
Justin Marwad <justinmarwad@gmail.com>
2023-07-10 18:07:47 -0400

    2023 update

--- BallotPush.py
+++ BallotPush.py
@@ -1,19 +1,22 @@
-# Made by Justin Marwad, 2022 
+# Made by Justin Marwad, 2022 (updated 2023)
 
 from dotenv import load_dotenv
 from redmail import gmail
 from pathlib import Path
-import csv, os, glob
+import csv, os, glob, json
+
+# FORMAT = "LastF_Round_X"
 
 class BallotPush: 
     def __init__(self, csv, subject, message, email=None, password=None): 
-        self.subject = subject
-        self.message = message
-        self.email = email 
-        self.password = password
+        self.subject    = subject
+        self.message    = message
+        self.email      = email 
+        self.password   = password
 
         self.data = open(csv) 
 
+        # Load .env file if email and password are not provided
         load_dotenv()
         if self.email is None: 
             self.email = os.getenv("EMAIL")
@@ -21,43 +24,139 @@ class BallotPush:
             self.password = os.getenv("PASSWORD")
         
 
-    def create_folders(self, dirname=None):
+    def collect_ballots(self):
+        """ Creates Python dictionary where the key is debater's lastname_firstname and each value is a list which contains two lists. One for full paths for pdf files which are ballots. And one for firstname, lastname, and email."""
+
+        print("[*] Collecting ballots...")
+
+        # Create empty dictionary
+        self.ballots = {}
+
+        # Loop over each row in the csv file
         for row in csv.reader(self.data, delimiter=","):
-            first_name = row[0] 
-            last_name = row[1] 
-            email = row[3]
-            
-            if dirname is None: 
-                dirname = f"{last_name}_{first_name}"
+            firstname, lastname, email = row[0], row[1], row[2] 
 
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
+            print(f"[*] Collecting ballots for {firstname} {lastname}...")
 
-    def send_ballot(self, first_name, last_name, email, full_message=None, files=None): 
-        file_attachments = []
-        for f in files:
-            file_attachments.append(Path(f))
+            # Parse ballots for the debater
+            parsed_ballots = self.parse_ballots(firstname, lastname)
+            if parsed_ballots: 
+
+                # Add the debater to the dictionary
+                self.ballots[f"{lastname}_{firstname}"] = [
+                    [firstname, lastname, email], 
+                    parsed_ballots
+                ]
+
+        return self.ballots
+
+    def parse_ballots(self, firstname, lastname): 
+        """ Finds a ballot given a name 
+                - Enters each round folder in the output folder (named Round_X)
+                - There are four rounds which are numbered 1-4 and additionally there are doubleoctas, octas, quarters, semis, and finals (though these may not exist)
+                - Runs parse_ballot on each ballot in each round folder 
+                - parse_ballot returns true or false depending on whether the ballot was found for the given name
+        """
+        # Create empty list
+        found_ballots = []
+
+        # Loop over each round folder in the output folder
+        for round_folder in glob.glob("output\\Round_*"):
+            # Get the round number from the folder name
+            round_num = round_folder.split("_")[-1]
+
+            # Loop over each ballot in the round folder
+            for ballot_filename in glob.glob(f"{round_folder}\\*.pdf"):
+
+
+                # Remove full path (i.e output\Round_X\) from ballot filename
+                ballot_filename = ballot_filename.split("\\")[-1]
+
+                # Parse the ballot
+                if self.parse_ballot(ballot_filename, firstname, lastname): 
+                    # If the ballot matches, add it to the list
+                    found_ballots.append(os.path.join(round_folder, ballot_filename))
+
+        return found_ballots
+
+    def parse_ballot(self, filename, firstname, lastname): 
+        """ Parses a ballot filename given a firstname and lastname and return True if matches, else return false ."""
+
+        # print(f"[*] Parsing ballot {filename}...")
+
+        # Split the string into three parts based on underscores
+        parts = filename.split('_')
+
+        # Extract the last name and first initial of each debater
+        debater1_last_name = parts[0][:-1]
+        debater1_first_initial = parts[0][-1]
+
+        debater2_last_name = parts[1][:-1]
+        debater2_first_initial = parts[1][-1]
+
+        # Print the results
+        # print("Debater 1:")
+        # print("Last Name:", debater1_last_name)
+        # print("First Initial:", debater1_first_initial)
+
+        # print("\nDebater 2:")
+        # print("Last Name:", debater2_last_name)
+        # print("First Initial:", debater2_first_initial, "\n\n")
+
+        # if debater1 or debater2 is the name we are looking for, return true
+        if debater1_last_name == lastname and debater1_first_initial == firstname[0].upper():
+            return True
+        elif debater2_last_name == lastname and debater2_first_initial == firstname[0].upper():
+            return True
+        else:
+            return False
+
+    def send_ballots(self):
+        """ Sends ballots to each debater in the ballots dictionary. """
+
+        print("[*] Sending ballots...")
+
+        # Loop over each debater in the ballots dictionary
+        for debater in self.ballots: 
+
+            # Get the debater's name, email, and ballots 
+            firstname   = self.ballots[debater][0][0]
+            lastname    = self.ballots[debater][0][1]
+            email       = self.ballots[debater][0][2]
+            files       = self.ballots[debater][1]
+
+            # Send the ballots
+            self.send_ballot(firstname, lastname, email, files)
+
+    def send_ballot(self, firstname, lastname, email, files=None, full_message=None): 
+        """ Sends a ballot to a debater. """
+        print(f"[*] Sending {firstname} {lastname}'s ballots to {email}...")
+        
+        # Collect all ballots and save them to list as Path objects 
+        file_attachments = [Path(f) for f in files]
 
         if full_message is None: 
-            full_message = f"Hello {first_name} {last_name}! <br><br> {self.message}"
+            full_message = f"Hello {firstname} {lastname}, <br><br> {self.message}"
 
-        gmail.username = self.username
-        gmail.password = self.password 
-        gmail.send(subject=self.subject, html=full_message, receivers=[email], attachments=file_attachments)
-
-    def send_ballots(self): 
-        for row in csv.reader(self.data, delimiter=","):
-            first_name = row[0]; last_name = row[1]; email = row[3]
-            files = glob.glob(f"{last_name}_{first_name}/*.pdf")
-
-            self.send_ballot(first_name, last_name, email, files)
+        gmail.username = self.email
+        gmail.password = self.password
+        gmail.send(
+            subject=self.subject, 
+            html=full_message, 
+            receivers=[email], 
+            attachments=file_attachments
+        )
 
 
 if __name__ == "__main__": 
-    bp = BallotPush()
+    bp = BallotPush(
+        "output/competitors.csv",
+        "Ballot Delivery - Tournament Name Year",
+        "Thank you for your participation in the {tournament} {year}! Your ballots have been attached to this email. <br><br> Regards, <br> {name} <br> {organization} <br>"  
+    )
 
-    # Create folders
-    bp.create_folders()
+    # create list
+    bp.collect_ballots() 
 
     # Send ballots
     bp.send_ballots()
--- README.md
+++ README.md
@@ -4,6 +4,30 @@
 
 The purpose of this project is to create a quick automated system of sending out ballots stored as PDFs in folders named after the competitors. 
 
-The create_folders.py script will create directories from a spreadsheet of competitor names. The send_ballots.py will email them to those competitors when the ballots are scanned and ready to go in the folders.
+In the "output" folder, folders are created to the format of "Round_X" for each round - where X could be 1, 2, 3, etc or could be "Finals" or "Semis" or "Quarters" etc. Inside each folder, ballots are named to the format "FirstL_FirstL_Round_X.pdf" where the first "FirstL" is the first debater and the second "FirstL" is the second debater.
 
 
+## Usage 
+
+### Setting Up Email 
+
+Follow the redmail instructions here: https://red-mail.readthedocs.io/en/stable/tutorials/config.html#config-gmail 
+
+In short, you'll need to (1) Enable 2FA and (2) Create an app password. Then, rename the example.env file to just .env and fill in the appropriate information (make sure the password field is your app password). 
+
+### Notes
+
+#### Separate PDFs
+
+The PDFs must be separate files. If they are not, you can use the following command to separate them (and use choco or apt to install "pdftk"):
+
+```
+pdftk Round_X.pdf burst output round_X_page_%04d.pdf
+```
+
+If you need to rotate pages then use: 
+    
+```
+pdftk Round_X.pdf cat 1-endeast output Round_X_Rotated.pdf
+pdftk Round_X_Rotated.pdf burst output Round_X/round_X_page_%01d.pdf  
+```
